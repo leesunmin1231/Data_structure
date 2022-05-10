fab49fd84f8ff9cea8924d98af1a1197287332a8
이선민 <sunmin@iseonmin-ui-MacBookPro.local>
2022-05-10 17:39:09 +0900

    save

--- Assignment/assignment_5/heapsort.c
+++ Assignment/assignment_5/heapsort.c
@@ -3,44 +3,43 @@
 
 #define ARR_SIZE 20000
 
-void adjust(char arr[ARR_SIZE][12], int root, int n)
-{
-	int child;
-	char tmp[20];
-	char rootkey[20];
-       	strcpy(tmp, arr[root]);
-	strcpy(rootkey, arr[root]);
-	child = 2*root;
-	while (child <= n)
-	{
-		if ((child < n) && (strcmp(arr[child], arr[child + 1]) < 0))
-			child++;
-		if(strcmp(rootkey, arr[child]) > 0)
-			break;
-		else
-		{
-			if (child/2 != child)
-				strcpy(arr[child/2], arr[child]);
-			child *= 2;
-		}
-	}
-	strcpy(arr[child/2], tmp);
+void heapify(char arr[ARR_SIZE][12], int here, int size) {
+	int left = here * 2 + 1;
+    	int right = here * 2 + 2;
+    	char temp[12];
+    	int max=here;
+    	if (left < size && strcmp(arr[left],arr[max]) > 0)
+        	max = left;
+    	if (right < size && strcmp(arr[right],arr[max]) > 0)
+        	max = right;
+
+    	if (max != here) {
+		strcpy(temp, arr[here]);
+		strcpy(arr[here], arr[max]);
+		strcpy(arr[max], temp);
+        	heapify(arr, max, size);
+    	}
 }
 
-void heap_sort(char arr[ARR_SIZE][12], int size)
-{
-	int i;
-	char temp[20];
-	for (i = size/2; i > 0; i--)
-		adjust(arr, i, size);
-	for (i = size - 1; i > 0; i--)
+void buildHeap(char arr[ARR_SIZE][12], int size) {
+    	int i,j;
+    	for (i = size / 2 - 1; i >= 0; i--) {
+        	heapify(arr, i, size);
+    	}
+}
+
+void heap_sort(char arr[ARR_SIZE][12],int size) {
+    	int treeSize;
+    	char temp[12];
+    	buildHeap(arr, size);
+    	for (treeSize = size - 1; treeSize >= 0; treeSize--)
 	{
-		if (i+1 != 1)
-		{
-			strcpy(temp, arr[1]);
-			strcpy(arr[1], arr[i+1]);
-			strcpy(arr[i+1], temp);
-		}
-		adjust(arr, 1, i);
-	}
+		if (treeSize != 0)
+	    	{
+		    	strcpy(temp, arr[0]);
+		    	strcpy(arr[0], arr[treeSize]);
+		    	strcpy(arr[treeSize], temp);
+	    	}
+	    	heapify(arr, 0,treeSize);
+    	}
 }
